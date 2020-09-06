578d76b013f863779f22105f5878a4226e65e920
Ed Mason <mynameisprivate3@gmail.com>
2020-09-06 16:50:46 +0100

    Minor changes - v0.5

--- web/app/urls.py
+++ web/app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('b/', views.benchlist, name="bench-list"),
     path('create/', views.CreateBench, name="create"),
     path('update/<str:pk>/', views.UpdateBench, name="update"),
-    path('delete/<str:pk>/', views.DeleteBench, name="delete"),
+    #path('delete/<str:pk>/', views.DeleteBench, name="delete"),
 
     path('login/', views.loginPage, name="login"),
     path('signup/', views.registerPage, name="register"),
